817d4dc5be65b95bcaf83bdd7332eb7eccc185ab
aliamerj <aliamer19ali@gmail.com>
2023-12-03 11:37:20 +0300

    make the form responsive

--- app/(main)/create_profile/page.tsx
+++ app/(main)/create_profile/page.tsx
@@ -4,14 +4,14 @@ import { ProfileForm } from "./profile_form";
 
 export default function CreateProfile() {
   return (
-    <main className="mt-28 flex flex-col justify-evenly md:flex-row  ">
-      <div className="w-full px-4 py-4 md:w-4/12 md:px-5 ">
-        <h2 className="mb-4 text-center text-2xl font-bold text-foreground">
+    <main className="mt-5 gap-2 sm:flex sm:justify-center md:flex md:justify-between md:py-5">
+      <div className="w-full md:w-5/12 ">
+        <h2 className="my-5 mb-4 text-center text-3xl font-bold text-foreground md:pt-9">
           Create your profile
         </h2>
         <ProfileForm />
       </div>
-      <div className="mt-4 w-full px-4 py-4 md:mt-0 md:w-7/12 md:bg-red-200 md:px-5">
+      <div className="mt-4 w-full bg-red-200 px-4 py-4 md:mt-0 md:w-6/12 ">
         <ProfileDisplay />
       </div>
     </main>
